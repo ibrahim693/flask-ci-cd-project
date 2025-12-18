06c6658fe96aeb61d2e53c1cc33754bd40937f47
QaziSteel <muhammadubaidullahqazi2003@gmail.com>
2025-12-18 11:42:08 +0500

    Change

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ VERSION = os.environ.get("APP_VERSION", "1.0.0")
 
 @app.route('/')
 def hello_world():
-    return f'<h1>Recent Key Results after change</h1><p>Version: {VERSION}</p>'
+    return f'<h1>HI I AM MUHAMMAD IBRAHIM AND THIS IS MY DEVOPS PROJECT</h1><p>Version: {VERSION}</p>'
 
 if __name__ == '__main__':
     # Host 0.0.0.0 makes it accessible outside the container
