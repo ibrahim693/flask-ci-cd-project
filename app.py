e502a153a2670c238c8d562c6a9a350115ef5368
QaziSteel <muhammadubaidullahqazi2003@gmail.com>
2025-12-18 11:35:24 +0500

    Update on Premier League

--- app.py
+++ app.py
@@ -9,7 +9,9 @@ VERSION = os.environ.get("APP_VERSION", "1.0.0")
 @app.route('/')
 def hello_world():
     return f'<h1>Recent Key Resultskflgjdflkgjdlkfjgdfkljg</h1><p>Version: {VERSION}</p>'
-
+@app.route('/')
+def hello_wor1():
+    return f'<h1>ALKKSHdfkjsdhfkjsdhgkjdfhgjks</h1><p>Version: {VERSION}</p>'
 if __name__ == '__main__':
     # Host 0.0.0.0 makes it accessible outside the container
     app.run(debug=True, host='0.0.0.0', port=5000)
