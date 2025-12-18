1371ed34cf8c9bd7956db5101bc671d3b693995c
QaziSteel <muhammadubaidullahqazi2003@gmail.com>
2025-12-18 10:03:23 +0500

    Update on Premier League

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ VERSION = os.environ.get("APP_VERSION", "1.0.0")
 
 @app.route('/')
 def hello_world():
-    return f'<h1>Recent Key Results</h1><p>Manchester United 4–4 Bournemouth: A chaotic thriller at Old Trafford on Monday night that saw United drop points despite a high-scoring display.</p>''
+    return f'<h1>Recent Key Results</h1><p>Mancheste United 4–4 Bournemouth: A chaotic thriller at Old Trafford on Monday night that saw United drop points despite a high-scoring display.</p>''
 
 if __name__ == '__main__':
     # Host 0.0.0.0 makes it accessible outside the container
